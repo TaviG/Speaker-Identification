1961adde30bd5120632389407429e8d6cb5bf562
TaviG <you@example.com>
2022-10-31 20:25:55 +0200

    Conversion mp4 to wav

---
+++ mp42wav.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 31 19:50:19 2022
+
+@author: Tavi
+"""
+
+import os
+import sys
+
+
+inputdir = sys.argv[1]
+
+filenames = []
+
+for path, subdirs, files in os.walk(inputdir):
+    for name in files:
+        filenames.append(os.path.join(path, name))
+        
+for filename in filenames:
+    if(filename.endswith(".mp4")):
+        os.system('ffmpeg -i {} -acodec pcm_s16le -ar 16000 {}.wav'.format(filename, filename[:-4]))
+    else:
+        continue
+
+print("Done")
