cabf7250480a62f6acaec0b25ca891834fde5547
horia.alex10 <horia.alex10@gmail.com>
2022-11-03 22:00:49 +0200

    Split dataset into X_train, Y_train, X_test, Y_test

--- main.py
+++ main.py
@@ -7,27 +7,36 @@ Created on Thu Nov  3 18:10:17 2022
 
 import os
 import sys
+import random
 
 
 inputdir = sys.argv[1]
 
 people = []
+audios = []
 
 for folder in os.listdir(inputdir):
-    people.append(os.path.join(inputdir, folder))
+    for video in os.listdir(os.path.join(inputdir, folder)):
+        for audio in os.listdir(os.path.join(inputdir, folder, video)):
+            if(audio.endswith(".wav")):
+                audios.append(os.path.join(inputdir, folder, video, audio))
+                people.append(folder)
+
+# Shuffle both arrays in the same way
+both = list(zip(audios, people))
+random.seed(42)
+random.shuffle(both)
+# Get back arrays
+audios, people = zip(*both)
+
+n_audios = len(audios)
+
+# Split into 80% train 20% test
+n_train = round(n_audios * 0.8)
     
-train = []
-test = []
+X_train = audios[:n_train]
+Y_train = people[:n_train]
 
-for person in people:
-   l = os.listdir(person)
-   for i in l[:int(0.8 * len(l))]:
-       train.append(i)
-   for i in l[int(0.8 * len(l)):]:
-       test.append(i)
-
-print("train:")
-print(train)
-print("test:")
-print(test)
+X_test = audios[n_train:]
+Y_test = people[n_train:]
         
